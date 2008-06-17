f7d34b9d05ecdbffd54348bd59e41b84db8de342
sparks.m <sparks.m@fc4cd32a-644a-0410-ad95-f3a6bd07964c>
2008-06-17 23:58:44 +0000

    Basic support for storing and mapping users to/from session ids

---
+++ CookieJar.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+import anydbm
+
+import md5
+
+class NoSuchUser(Exception): pass
+
+cookiesFile = "/data/Cookies/Cookies.dbm"
+
+def _newCookie(Cookies):
+    try:
+        highest = Cookies["meta:highest"]
+    except KeyError:
+        highest = "1"
+    Cookies["meta:highest"] = repr(int(highest) + 1)
+    return highest
+
+
+def getCookie(userid):
+    Cookies = anydbm.open(cookiesFile, "c")
+    try:
+       cookie = Cookies["user:"+userid]
+    except KeyError:
+       cookie = md5.md5(_newCookie(Cookies)).hexdigest()
+       Cookies["user:"+userid] = cookie
+       Cookies["cookie:"+cookie] = userid
+    Cookies.close()
+    return cookie
+    
+def getUser(cookie):
+    Cookies = anydbm.open(cookiesFile, "c")
+    try:
+        user = Cookies["cookie:"+cookie]
+    except KeyError:
+        Cookies.close()
+        raise NoSuchUser(cookie)
+    Cookies.close()
+    return user
+
+def zapCookies():
+    Cookies = anydbm.open(cookiesFile, "n")
+    Cookies.close()
+
+if __name__ == "__main__":
+    # Basic acceptance test
+    user = "0"
+    cookie = getCookie("0")
+    print cookie
+    user_ = getUser(cookie)
+    assert user_ == user
+    print user_
+    zapCookies()
+    try:
+        user_ = getUser(cookie)
+        raise "Failed to zapCookies"
+    except NoSuchUser:
+        # Success
+        pass
