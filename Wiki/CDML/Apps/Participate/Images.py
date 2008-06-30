58d7cdcf74b5e66677c500f790a07b8250b90e45
sparks.m <sparks.m@fc4cd32a-644a-0410-ad95-f3a6bd07964c>
2008-06-30 00:19:51 +0000

    Added in support for looking at a single image as a single image viewer Changed image previews to link to a given single image viewer provided support for browsing all user images Added note to uploads about how long they may take
    Michael

--- Wiki/CDML/Apps/Participate/Images.py
+++ Wiki/CDML/Apps/Participate/Images.py
@@ -48,12 +48,38 @@ Upload file: <input type="file" name="upload.filename" value="" size="30"/>
 
 <input type="submit" value="submit" />
 
-</form>
+</form><br>
+<b> Please note - after you hit submit the amount of time taken to upload
+may be significant - at least a few minutes. For very hi resoultion images,
+it may be as much as 10-15 minutes. Please be patient!</b>
+
+
 """
 
 class tagHandler(object):
       def doUploadForm(bunch, text, env):
           return upload_form
+
+      def doAllUserImages(bunch, text, env):
+          user_images = Images.read_database()
+          images = []
+          i = 0
+          for image in user_images:
+              i = i +1
+              if i % 4 == 0:
+                   image["extra"] = " last"
+              else:
+                   image["extra"] = ""
+              image = """\
+<div class='column twoC%(extra)s button'>
+<a href="/Picture?image=%(unique_name)s">
+<img border="0" src='/images/user/%(unique_name)s/thumb.jpg'>
+</a></div>
+""" % image
+              if i % 4 == 0:
+                  image += '<div class="divide"></div>'
+              images.append(image)
+          return "".join(images)+ '<div class="divide"></div>'
 
       def doUserImages(bunch, text, env):
           userid = loggedIn(env)
@@ -65,17 +91,51 @@ class tagHandler(object):
                       user_images.append(image)
               Y = [ x["unique_name"] for x in user_images]
               images = []
+              i = 0
               for image in user_images:
-                  image = "<img class='column twoC button' src='/images/user/%(unique_name)s/thumb.jpg'>" % image
+                  i = i +1
+                  if i % 4 == 0:
+                       image["extra"] = " last"
+                  else:
+                       image["extra"] = ""
+                  image = """\
+<div class='column twoC%(extra)s button'>
+<a href="/Picture?image=%(unique_name)s">
+<img border="0" src='/images/user/%(unique_name)s/thumb.jpg'>
+</a></div>
+""" % image
+                  if i % 4 == 0:
+                      image += '<div class="divide"></div>'
                   images.append(image)
               return "".join(images)+ '<div class="divide"></div>'
               return repr(Y)
           else:
               return "can't give you images, you're not logged in"
           
+      def doOnePictureViewer(bunch, text, env):
+          image = bunch.get("image", text)
+          if ".." in "image":
+              return ""
+          return """\
+<div class="divide"></div>
+                <div class="column oneC">
+                <p> &nbsp;
+                </p></div>
+                <div class="column sixC lightgrey">
+<img src="http://bicker.kamaelia.org/images/user/%(image)s/normal.jpg">
+                </div>
+                <div class="column last oneC">
+                <p> &nbsp;
+
+                </p></div>
+<div class="divide"></div>
+""" % { "image" : image }
+
       mapping = {
+           "onepictureviewer" : doOnePictureViewer,
            "uploadone_form" : doUploadForm,
            "userimages" : doUserImages,
+           "alluserimages" : doAllUserImages,
       }
 
 mapping = tagHandler.mapping
@@ -84,8 +144,6 @@ if __name__ == "__main__":
    print "TAG HANDLER", tagHandler
    print "MAPPING", tagHandler.mapping
    print "HMM", tagHandler.mapping["w"]({"location":"bingle"}, "hello world", {})
-
-
 
    if 0:
           if loggedIn(env):
