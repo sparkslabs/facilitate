ba5bfa847ff748291df09733d1bfcfabfc796626
sparks.m <sparks.m@fc4cd32a-644a-0410-ad95-f3a6bd07964c>
2008-07-10 23:40:58 +0000

    Initial version of a setup.py file for the Facilitate codebase.
    Michael.

---
+++ app/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "Facilitate",
+      version = "0.1.0",
+      description = "Facilitate Data Backend",
+      author = "Michael Sparks",
+      author_email = "ms_@users.sourceforge.net",
+      url = "",
+      license = "Copyright (c)2008 BBC ... License : ",
+      packages = [\
+                  "Facilitate",
+                  "Facilitate.model",
+                  ""],
+#      scripts = [ "App/somescript" ],
+
+#      data_files=[ ('/etc', ['Config/facilitate.conf.dist']) ],
+
+      long_description = """
+""",
+      )
+
