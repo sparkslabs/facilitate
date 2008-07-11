cb8737e7469c33c85b128f48757aeee7bf5a3803
sparks.m <sparks.m@fc4cd32a-644a-0410-ad95-f3a6bd07964c>
2008-07-11 03:24:30 +0000

    Extracted from the CDML code that needs this information

---
+++ Facilitate/Api.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+from Facilitate.model.Record import EntitySet
+
+Images        = None
+Registrations = None
+Contacts      = None
+
+def initApi(basedir):
+    EntitySet.data = basedir + "/cgi/app/data"
+
+    global Images
+    global Registrations
+    global Contacts
+
+    Images        = EntitySet("images", key="imageid")
+    Registrations = EntitySet("registrations", key="regid")
+    Contacts      = EntitySet("contacts", key="contactid")
+
+def getRegistration(userid):
+    user = Registrations.get_record(userid)
+    return user
+
+def ContactsImages(contacts):
+#    raise repr(contacts)
+    images = Images.read_database()
+    user_images = []
+    userids = []
+    for image in images:
+        userids.append(image["userid"])
+        if image["userid"] in contacts:
+            user_images.append(image)
+    return user_images
+
+def getContacts(userid):
+    for rec in Contacts.read_database():
+        if rec["contactof"] == userid:
+            return rec["contacts"]
+    else:
+        return None
+
+def getAllImages():
+    return Images.read_database()
+
+def getUserImages(userid):
+    images = Images.read_database()
+    user_images = []
+    for image in images:
+        if image["userid"] == userid:
+            user_images.append(image)
+    return user_images
+
