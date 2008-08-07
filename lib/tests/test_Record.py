54e3cc182adca30a31fc9708d05790831f45c7af
sparks.m <sparks.m@fc4cd32a-644a-0410-ad95-f3a6bd07964c>
2008-08-07 18:26:54 +0000

    Added tests regarding getting, updating, deleting records Added facilities to read the DB Added facilities to check the key

--- lib/tests/test_Record.py
+++ lib/tests/test_Record.py
@@ -61,7 +61,254 @@ class Test_Record_new_record(unittest.TestCase):
             self.assertEqual( left["name"], right["name"] )
             self.assertEqual( left["age"], right["age"] )
 
-    
+class Test_Record_get_record(unittest.TestCase):
+    def setUp(self):
+        self.key = "personid"
+
+        Record.EntitySet.Zap(name="People", key=self.key)
+        self.X = Record.EntitySet(name="People", key=self.key)
+
+    def test_get_record_onerecord(self):
+        "Record.get_record - add record, get same record, should give same record."
+        newrec = self.X.new_record({"name": "bob", "age": 30})
+        self.assertEqual(newrec["personid"], "0")
+
+        stored_rec = self.X.get_record("0")
+        self.assertEqual( newrec["name"], stored_rec["name"] )
+        self.assertEqual( newrec["age"], stored_rec["age"] )
+        self.assertNotEqual( id(newrec), id(stored_rec) )
+
+    def test_get_record_multiplerecords(self):
+        "Record.get_record - add multiple records, get back records, should be same records "
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+        newrecs = []
+
+        for i in rec_raws:
+            self.X.new_record( i )
+
+        for i in "0", "1", "2", "3":
+             newrecs.append( self.X.get_record( i ) )
+
+        for left,right in zip(rec_raws, newrecs):
+            self.assertEqual( left["name"], right["name"] )
+            self.assertEqual( left["age"], right["age"] )
+
+    def test_get_record_multiplerecords_differentorder(self):
+        "Record.get_record - add multiple records, get back records, should be same records "
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+        expected_recs = [
+                    rec_raws[3], rec_raws[1], rec_raws[0], rec_raws[2],
+                  ]
+        newrecs = []
+
+        for i in rec_raws:
+            self.X.new_record( i )
+
+        for i in "3", "1", "0", "2":
+             newrecs.append( self.X.get_record( i ) )
+
+        for left,right in zip(expected_recs, newrecs):
+            self.assertEqual( left["name"], right["name"] )
+            self.assertEqual( left["age"], right["age"] )
+
+class Test_Record_update_record(unittest.TestCase):
+    def setUp(self):
+        self.key = "personid"
+
+        Record.EntitySet.Zap(name="People", key=self.key)
+        self.X = Record.EntitySet(name="People", key=self.key) # Put stuff in here
+        self.Y = Record.EntitySet(name="People", key=self.key) # Check here
+                                                               # Assumption that these are different :-)
+
+    def test_UpdateOneRecord(self):
+        "Record - update record. Create records. Get one, update it, store it, get it & check changed"
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+
+        for i in rec_raws:
+            self.X.new_record( i )
+
+        rec = self.X.get_record("2")
+        rec["age"] = str(int(rec["age"])+1)
+
+        self.X.store_record( rec )
+
+        rec = self.Y.get_record("2")
+        self.assertEqual( rec_raws[2]["name"], rec["name"])
+        self.assertNotEqual( rec_raws[2]["age"], rec["age"])
+        self.assertEqual( "33", rec["age"])
+
+    def test_UpdateMultipleRecords(self):
+        "Record - update many records. Create records. Get one, update it, store it, get it & check changed"
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+
+        for i in rec_raws:
+            self.X.new_record( i )
+
+        rec = self.X.get_record("0")
+        rec["age"] = "21"
+        rec["name"] = "alice smith"
+        self.X.store_record( rec )
+
+        rec = self.X.get_record("1")
+        rec["age"] = "29"
+        rec["name"] = "bob smith"
+        self.X.store_record( rec )
+
+        rec = self.X.get_record("2")
+        rec["age"] = "40"
+        rec["name"] = "charlie frank"
+        self.X.store_record( rec )
+
+        rec = self.X.get_record("3")
+        rec["age"] = "41"
+        rec["name"] = "dan frank"
+        self.X.store_record( rec )
+
+        rec = self.Y.get_record("0")
+        self.assertEqual(rec["name"], "alice smith")
+        self.assertEqual( rec["age"], "21")
+
+        rec = self.Y.get_record("1")
+        self.assertEqual(rec["name"], "bob smith")
+        self.assertEqual( rec["age"], "29")
+
+
+        rec = self.Y.get_record("2")
+        self.assertEqual(rec["name"], "charlie frank")
+        self.assertEqual( rec["age"], "40")
+
+
+        rec = self.Y.get_record("3")
+        self.assertEqual(rec["name"], "dan frank")
+        self.assertEqual( rec["age"], "41")
+
+
+class Test_Record_readDB(unittest.TestCase):
+    def setUp(self):
+        self.key = "personid"
+
+        Record.EntitySet.Zap(name="People", key=self.key)
+        self.X = Record.EntitySet(name="People", key=self.key) # Put stuff in here
+        self.Y = Record.EntitySet(name="People", key=self.key) # Check here
+
+    def test_readBD_oneRecord(self):
+        newrec = self.X.new_record({"name": "bob", "age": 1})
+        records = self.Y.read_database()
+        self.assertEqual(len(records), 1)
+        self.assert_( newrec in records )
+
+
+    def test_readBD_manyRecords(self):
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+        recs = []
+
+        for i in rec_raws:
+            recs.append( self.X.new_record( i ) )
+
+        records = self.Y.read_database()
+        
+        for rec in recs:
+            self.assert_( rec in records )
+
+class Test_Record_deleteRecord(unittest.TestCase):
+    def setUp(self):
+        self.key = "personid"
+
+        Record.EntitySet.Zap(name="People", key=self.key)
+        self.X = Record.EntitySet(name="People", key=self.key) # Put stuff in here
+        self.Y = Record.EntitySet(name="People", key=self.key) # Check here
+
+    def test_addonedeleteone(self):
+        "Record.delete_record - add a record and delete it, confirm gone and empty set"
+        newrec = self.X.new_record({"name": "bob", "age": 1})
+        self.X.delete_record( "0" )
+        records = self.Y.read_database()
+        self.assertEqual(len(records), 0)
+
+    def test_delete_addManyDeleteOne(self):
+        "Record.delete_record - add man records, delete one."
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+        recs = []
+        for i in rec_raws:
+            self.X.new_record( i )
+        
+        records = self.Y.read_database()
+        self.assertEqual(len(records), 4)
+        equalCount = 0
+        for record in records:
+            if record["name"] == "charlie":
+                equalCount += 1
+        self.assertEqual( equalCount, 1 )
+        
+        self.X.delete_record( "2" )
+        
+        records = self.Y.read_database()
+        self.assertEqual(len(records), 3)
+        
+        for record in records:
+            self.assertNotEqual( "charlie", record["name"] )
+
+    def test_delete_addManyDeleteAll(self):
+        "Record.delete_record - add man records, delete one."
+        rec_raws = [
+                      {"name": "alice",   "age": "30"},
+                      {"name": "bob",     "age": "31"},
+                      {"name": "charlie", "age": "32"},
+                      {"name": "dan",     "age": "33"},
+                  ]
+        recs = []
+        for i in rec_raws:
+            self.X.new_record( i )
+        
+        records = self.Y.read_database()
+        self.assertEqual(len(records), 4)
+
+        for key in [ "0", "1", "2", "3" ]:
+            self.X.delete_record( key )
+        
+        records = self.Y.read_database()
+        self.assertEqual(len(records), 0)
+
+class Test_Record_key(unittest.TestCase):
+    def setUp(self):
+        self.key = "personid"
+
+        Record.EntitySet.Zap(name="People", key=self.key)
+        self.X = Record.EntitySet(name="People", key=self.key)
+
+    def test_delete_addManyDeleteAll(self):
+        "Record.key - returns the key for this entity"
+        self.assertEqual(self.key, self.X.key() )
 
 
 if __name__=="__main__":
